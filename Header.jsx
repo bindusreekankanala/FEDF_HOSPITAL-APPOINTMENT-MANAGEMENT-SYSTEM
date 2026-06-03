8b9ccfdc2fda45fcda9ba35bc00012ce64c8bfb6
bindusreekankanala <bindusree.kankanala@gmail.com>
2026-06-03 08:37:16 +0530

    Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,17 @@
+Header.jsx
+function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        padding: "15px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Doctor Appointment Booking System</h1>
+    </header>
+  );
+}
+
+export default Header;
