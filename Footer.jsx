0367f568590001a75d8a4aa003ebc50baf1e1885
bindusreekankanala <bindusree.kankanala@gmail.com>
2026-06-03 08:38:08 +0530

    Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,18 @@
+Footer.jsx
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <p>© 2026 Doctor Appointment System</p>
+    </footer>
+  );
+}
+
+export default Footer;
