dd97e11eee761af181e755ebb0dd05d73eca83ff
bindusreekankanala <bindusree.kankanala@gmail.com>
2026-06-03 08:40:08 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+App.jsx
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Login />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
